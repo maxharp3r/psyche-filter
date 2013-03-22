5a2eecd2a2b08f7f5fe97a4053d7415a48a68687
Max Harper <maxharp3r@gmail.com>
2013-03-22 17:31:32 -0500

    Initial commit - mostly html5 boilerplate and express

---
+++ survey-app/README.md
@@ -0,0 +1,20 @@
+
+Running
+=======
+
+To install:
+
+    (install node and npm)
+    npm install
+    cp config-sample.json config.json
+    # edit config.json
+
+To start a server:
+
+    npm start
+
+
+Docs
+====
+
+
---
+++ survey-app/app.js
@@ -0,0 +1,42 @@
+// external dependencies
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+// local dependencies
+var config = require('./config.json');
+
+var app = express();
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.set('view options', { layout: false, debug: true, pretty: true });
+    app.set('strict routing', true);
+    app.use(express['static'](path.join(__dirname, 'public')));
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+
+    app.use(express.bodyParser());
+    app.use(express.cookieParser('aofineknasldifahd'));
+    app.use(express.session({ key: 'sid', cookie: { maxAge: config.cookie_max_age }}));
+    app.use(express.methodOverride());
+    app.use(app.router);
+});
+
+app.configure('development', function() {
+    app.use(express.errorHandler());
+});
+
+
+app.get('/survey', function(req, res) {
+    res.sendfile('./public/index.html');
+});
+
+// Angular app
+//app.get('/', routes.angular_app);
+//app.get('/domains/*', routes.angular_app);
+
+
+http.createServer(app).listen(config.bind.port, config.bind.host, function () {
+    console.log("Express server listening on http://" + config.bind.host + ":" + config.bind.port + "/");
+});
