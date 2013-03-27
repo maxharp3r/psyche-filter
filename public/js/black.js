9628d8cc2e4d8bfd72c911ec28220104e2c1ba99
Max Harper <maxharp3r@gmail.com>
2013-03-26 21:35:15 -0500

    Add "stop here" functionality to demo

--- public/js/black.js
+++ public/js/black.js
@@ -7,30 +7,41 @@ BLACK.DELAY_MS = 100;
 BLACK.ids = ['foo', 'bar', 'baz'];
 
 BLACK.turn_on = function(css_id) {
-	return function() {
-		$('#' + css_id).addClass('on');
-	}
+    return function() {
+        $('#' + css_id).addClass('on');
+    }
 }
 
 BLACK.turn_off = function(css_id) {
-	return function() {
-		$('#' + css_id).removeClass('on');
-	}
+    return function() {
+        $('#' + css_id).removeClass('on');
+    }
 }
 
 
-BLACK.demo = function() {
-	_.each(BLACK.ids, function(id, idx) {
-		var time = BLACK.DELAY_MS * idx;
-		setTimeout(BLACK.turn_on(id), time);
-		setTimeout(BLACK.turn_off(id), time + BLACK.DELAY_MS);
-	});
-	
+BLACK.demo = function(last_id) {
+    $('.spotlight').removeClass('on');
+
+    _.any(BLACK.ids, function(id, idx) {
+        var time = BLACK.DELAY_MS * idx;
+
+        if (id !== last_id) {
+            console.log("id:", id);
+            setTimeout(BLACK.turn_on(id), time);
+            setTimeout(BLACK.turn_off(id), time + BLACK.DELAY_MS);
+        } else {
+            // stop here
+            setTimeout(BLACK.turn_on(id), time);
+            return true;
+        }
+        return false;
+    });
+
 };
 
 // main()
 $(function() {
-	$(document).click(function (event) {    		
-	    BLACK.demo();
-	});
+    $(document).click(function (event) {
+        BLACK.demo('bar');
+    });
 });
