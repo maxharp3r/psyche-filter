e6cb0bd8300be8b511320ad1b5cc889e7c05059b
Max Harper <maxharp3r@gmail.com>
2013-04-14 22:24:44 -0500

    print!

--- arduino/coupons/coupons.ino
+++ arduino/coupons/coupons.ino
@@ -1,51 +1,52 @@
 /* 
 
-Web_HelloWorld.pde - very simple Webduino example 
+ Web_HelloWorld.pde - very simple Webduino example 
 
-curl -X POST --data "text=here is a nifty coupon" http://192.168.0.38/c
-
-Docs:
-* https://github.com/sirleech/Webduino/
-* https://www.adafruit.com/products/597#Downloads
+ curl -X POST --data "text=here is a nifty coupon" http://192.168.0.38/c
  
+ Docs:
+ * https://github.com/sirleech/Webduino/
+ * https://www.adafruit.com/products/597#Downloads
+     * http://learn.adafruit.com/mini-thermal-receipt-printer/
+ * wiring
+     * http://bildr.org/2011/08/thermal-printer-arduino/
  */
 
 #include "SPI.h"
 #include "Ethernet.h"
-#include "WebServer.h"
 
+// external libs
+#include "WebServer.h"
+#include "SoftwareSerial.h"
+#include "Adafruit_Thermal.h"
+
+
+// web server setup
 static uint8_t mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0xA7, 0x3D };
 static uint8_t ip[] = { 192, 168, 0, 38 };
-int led = 13;
-
-
-/* This creates an instance of the webserver.  By specifying a prefix
- * of "", all pages will be at the root of the server. */
 #define PREFIX ""
 WebServer webserver(PREFIX, 80);
-
-// messages (read only memory)
-P(helloMsg) = "<h1>Hello, World!</h1>";
-P(couponMsg) = "<h1>Give me a coupon.</h1>\n";
-
 #define NAMELEN 32
 #define VALUELEN 256
 
 
+// printer setup
+int printer_RX_Pin = 5;  // This is the green wire
+int printer_TX_Pin = 6;  // This is the yellow wire
+Adafruit_Thermal printer(printer_RX_Pin, printer_TX_Pin);
+
+
+// web server messages (read only memory)
+P(helloMsg) = "<h1>Hello, World!</h1>";
+P(couponMsg) = "<h1>Give me a coupon.</h1>\n";
+
+
 void helloCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
   server.httpSuccess();
-
   if (type == WebServer::HEAD) {
     return;
   }
-  
   server.printP(helloMsg);
-  
-//  Serial.println("high");
-//  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-//  delay(1000);               // wait for a second
-//  Serial.println("low");
-//  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
 }
 
 void couponCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) {
@@ -55,13 +56,10 @@ void couponCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) 
     return;
   }
   
-  server.httpSuccess();
-  
   char name[NAMELEN];
   char value[VALUELEN];
-  
+  server.httpSuccess();
   server.printP(couponMsg);
-  
   while (server.readPOSTparam(name, NAMELEN, value, VALUELEN)) {
     server.print("\n");
     server.print(name);
@@ -70,6 +68,12 @@ void couponCmd(WebServer &server, WebServer::ConnectionType type, char *, bool) 
     server.print("\n");
   }
   
+  // test print
+  printer.wake();
+  printer.setDefault();
+  printer.println("hello, world!");
+  printer.feed(2);
+  printer.sleep();
 }
 
 void setup() {
@@ -79,9 +83,11 @@ void setup() {
   webserver.addCommand("c", &couponCmd);
   webserver.begin();
   
-//  pinMode(led, OUTPUT);
   Serial.begin(9600);
   Serial.println("starting web server");
+  
+  pinMode(7, OUTPUT); digitalWrite(7, LOW); // To also work w/IoTP printer
+  printer.begin();
 }
 
 void loop() {
