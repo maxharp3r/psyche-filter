7dcd4ff7cc8898a243f9683c0ccc2dc4feb94aa8
Max Harper <maxharp3r@gmail.com>
2013-04-14 20:49:21 -0500

    Use webduino

---
+++ arduino/coupons/coupons.ino
@@ -0,0 +1,96 @@
+/*
+  Coupons app
+  
+  $ curl -X GET http://192.168.0.38/print
+  
+  Docs:
+  * http://arduino.cc/en/Reference/Ethernet
+  
+  Circuit:
+  * Ethernet shield attached to pins 10, 11, 12, 13
+ */
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+byte mac[] = {  
+  0x90, 0xA2, 0xDA, 0x0D, 0xA7, 0x3D };
+IPAddress ip(192, 168, 0, 38);
+
+// Initialize the Ethernet server library
+// with the IP address and port you want to use 
+// (port 80 is default for HTTP):
+EthernetServer server(80);
+
+void setup() {
+ // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo only
+  }
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+}
+
+
+void loop() {
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          // client.println("<meta http-equiv=\"refresh\" content=\"5\">");
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");       
+          }
+          client.println("</html>");
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    // close the connection:
+    client.stop();
+    Serial.println("client disonnected");
+  }
+}
+
+void sendResponse(EthernetClient client) {
+}
+
+
