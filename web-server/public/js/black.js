496a66fb5111666f1c675d3842c11a96f581ed98
Max Harper <maxharp3r@gmail.com>
2013-04-18 21:38:31 -0500

    add all four pages of words

--- web-server/public/js/black.js
+++ web-server/public/js/black.js
@@ -4,7 +4,11 @@ var BLACK = {};
 
 BLACK.DELAY_MS = 100;
 
-BLACK.ids = ['absent-minded','accommodating','accurate','agitated','agreeable','aloof','altruistic','anti-intellectual','antisocial','anxious','argumentative','ashamed','assertive','blunt','brassy','callous','calm','carefree','careless','chaotic','cheerful','civic-minded','civil','clever','closed-minded','cocky','cold','collected','compassionate','competent','composed','confident','conforming','conscientious','conservative','considerate','controls_emotions','conventional','cool-headed','cooperative','creative','cynical','daydreamer','defiant','depressive','detail_oriented','dirty','disagreeable'];
+BLACK.ids0 = ['absent-minded','accommodating','accurate','agitated','agreeable','aloof','altruistic','anti-intellectual','antisocial','anxious','argumentative','ashamed','assertive','blunt','brassy','callous','calm','carefree','careless','chaotic','cheerful','civic-minded','civil','clever','closed-minded','cocky','cold','collected','compassionate','competent','composed','confident','conforming','conscientious','conservative','considerate','controls_emotions','conventional','cool-headed','cooperative','creative','cynical','daydreamer','defiant','depressive','detail_oriented','dirty','disagreeable'];
+BLACK.ids1 = ['disciplined','disenchanted','disheveled','distant','distracted','do-gooder','dogmatic','dominant','driven','dutiful','efficient','emotional','empathetic','enthusiastic','erratic','exit','friendly','fun-loving','generous','gracious','gruff','guarded','haphazard','happy','hard-nosed','hardened','hasty','headstrong','helpful','hopeful','hostile','hot-tempered','imaginative','imposing','impromptu','impulsive','inconsiderate','inconspicuous','indifferent','indulgent','industrious','inflexible','ingenious','innovative','institutional','insulting','intellectual','introspective'];
+BLACK.ids2 = ['irritable','kind-hearted','lackadaisical','law_abiding','lax','liberal','mannerly','meek','methodical','mindless','misanthropic','modest','moody','mousy','musical','narrow-minded','nature_lover','neurotic','nurturing','open','open-minded','optimistic','orderly','organized','orthodox','outgoing','overwhelmed','passive-aggressive','perfectionist','persuasive','polite','practical','pragmatic','proud','purposeful','quick_thinker','quiet','rash','rational','receptive','relaxed','relentless','resilient','restrained','rigid','rude','sassy','scatter-brained'];
+BLACK.ids3 = ['scruffy','self-assured','self-conscious','self-contained','self-controlled','self-involved','self-reflective','serene','sexual','short-sighted','shy','sloppy','sorrowful','spirited','spontaneous','stable','staunch','stodgy','straightforward','structured','stubborn','submissive','suspicious','systematic','tactless','takes_charge','talkative','tender','tidy','timid','tough','traditional','trusting','uncharitable','unenthusiastic','unforgiving','unprepared','unstable','utilitarian','vivacious','volatile','vulnerable','warm','well-groomed','withdrawn','witty','worrisome','zesty'];
+
 
 BLACK.turn_on = function(css_id) {
     return function() {
@@ -22,7 +26,7 @@ BLACK.turn_off = function(css_id) {
 BLACK.demo = function(last_id) {
     $('.spotlight').removeClass('on');
 
-    _.any(BLACK.ids, function(id, idx) {
+    _.any(BLACK.ids0, function(id, idx) {
         var time = BLACK.DELAY_MS * idx;
 
         if (id !== last_id) {
