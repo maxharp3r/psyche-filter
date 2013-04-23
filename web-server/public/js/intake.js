ddc68ee425e6794f89f77d1558b8024c649bbfc9
Max Harper <maxharp3r@gmail.com>
2013-04-22 20:22:30 -0500

    Add scrolling effect and first few survey screens

--- web-server/public/js/intake.js
+++ web-server/public/js/intake.js
@@ -1,11 +1,20 @@
 
+function scrollToAnchor(aid) {
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: $('#' + aid).offset().top}, 'slow');
+}
+
 function IntakeCtrl($scope, $http, $location, $anchorScroll) {
+
     $scope.scrollTo = function(id) {
-        $location.hash(id);
-        $anchorScroll();
+        scrollToAnchor(id);
+        // this is the native angular way to scroll
+        // $location.hash(id);
+        // $anchorScroll();
     }
 
     $scope.submitSurvey = function() {
         console.log("process survey", $scope.survey);
     }
 }
+
